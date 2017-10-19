67faca5bd22a3e1c9c60eeb6a1fa540bbf4955f3
wieliczkaPG <p.grodzicki81@gmail.com>
2017-10-19 15:54:28 +0200

    5 program

---
+++ prog5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int mnoz(int a, int b) {return a*b;}
+int dodaj(int a, int b) {return a+b;}
+
+
+int main(void)
+{
+
+	int a = 2, b=5;   /* deklaracja wraz z inicjalizacja */
+	char wybor;
+
+	scanf("%c", &wybor); /* czytaj z konsoli i zapisz warttosc pod wybor */
+
+/*if (wyrazenie) {blok jesli wyrazenie prawdziwe} else {blok jesliu wyrazenie falszywe}*/
+
+
+	if(wybor == '*') 
+{
+	
+	printf("wynik mnozenia: %i\n", mnoz(a,b));
+
+}else{
+
+	printf("wynik dodawania: %i\n", dodaj(a,b));
+};
+
+
+	putchar('\n');  /*znak konca lini*/
+
+	return 0;
+}
